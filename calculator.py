51e1b3cbda19eb34fdf595593ca480ff0a418f8b
Niklas Kondracki <niklas.kndr@gmail.com>
2026-05-13 09:18:13 +0200

    Refactor calculator to use a function for operations

--- calculator.py
+++ calculator.py
@@ -1,15 +1,21 @@
-nummer1 = float(input("Gib die 1. Nummer ein: "))
-nummer2 = float(input("Gib die 2. Nummer ein: "))
-operation = input("Gib die Operation ein (+, -, *, /): ")
+def rechnen(nummer1, nummer2, operation):
+    if operation == "+":
+        return nummer1 + nummer2
+    elif operation == "-":
+        return nummer1 - nummer2
+    elif operation == "*":
+        return nummer1 * nummer2
+    elif operation == "/":
+        return nummer1 / nummer2
+    else:
+        return "Falscher Operator"
 
-if operation == "+":
-    ergebnis = nummer1 + nummer2
-elif operation == "-":
-    ergebnis = nummer1 - nummer2
-elif operation == "*":
-    ergebnis = nummer1 * nummer2
-elif operation == "/":
-    ergebnis = nummer1 / nummer2
-else:    ergebnis = "Falscher Operator"
+for i in range (10):
+    nummer1 = float(input("Gib die 1. Nummer ein: "))
+    nummer2 = float(input("Gib die 2. Nummer ein: "))
+    operation = input("Gib die Operation ein (+, -, *, /): ")
 
-print(f"Ergebnis: {ergebnis}")
+    ergebnis = rechnen(nummer1, nummer2, operation)
+    print(f"Ergebnis: {ergebnis}")
+
+rechnen()
