ebc692c06e259fbce35f0866fc41c2f88f551292
Niklas Kondracki <niklas.kndr@gmail.com>
2026-05-21 13:45:06 +0200

    Add price calculations and display in techworld_lists

--- techworld_lists.py
+++ techworld_lists.py
@@ -1,9 +1,30 @@
 produkte = ["Gaming-Laptop", "Wireless-Maus", "Mechanische Tastatur", "4K-Monitor", "Webcam"]
+
 print("████████████████████████████████████████████")
 print("███           T E C H W O R L D          ███")
 print("████████████████████████████████████████████")
 print()
+
 for nummer, artikel in enumerate(produkte, start=1):
     print(f"{nummer}. {artikel}")
+
 print("----")
 print(f"Anzahl Produkte: ",len(produkte))
+print()
+
+preise = [1299.00, 49.90, 129.00, 399.00, 89.90]
+gesamtpreis = sum(preise)
+durschnittspreis = sum(preise) / len(preise)
+min = min(preise)
+max = max(preise)
+
+print("Preisliste:")
+
+for nummer, preis in enumerate(preise, start=1):
+    print(f"{nummer}. {preis:.2f}€")
+
+print("----")
+print(f"Gesamtpreis: {gesamtpreis:.2f}€")
+print(f"Durchschnittspreis: {durschnittspreis:.2f}€")
+print(f"Geringster Preis: {min:.2f}€")
+print(f"Höchster Preis: {max:.2f}€")
